89242cac261cb0b3bb7063477cee839bf34cb8be
zyxwars <zyxwars221@gmail.com>
2023-01-28 13:29:54 +0100

    first commit

---
+++ client/src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import UploadModal from "./lib/UploadModal.svelte";
+</script>
+
+<main><UploadModal /></main>
---
+++ client/src/lib/UploadModal.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let filesToUpload = new Set<File>();
+</script>
+
+<div>
+  <input
+    type="file"
+    multiple
+    directory
+    webkitdirectory
+    mozdirectory
+    on:change={(e) => {
+      console.log(e.target.files);
+    }}
+  />
+
+  {#each Array.from(filesToUpload) as file}
+    <div>{file.name}</div>
+  {/each}
+</div>
