2344c9edd220a6b3d405ea380e39ee71a60f437a
zyxwars <zyxwars221@gmail.com>
2023-03-10 22:41:49 +0100

    fix persist

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -3,7 +3,7 @@ import path from "path";
 // TODO: 1000 * 60 * 60 * 24 * 7;
 export const DELETE_AFTER = 10000;
 export const PORT = 8080;
-export const UPLOADS_DIR = path.join(__dirname, "../persist/uploads");
+export const UPLOADS_DIR = path.join(__dirname, "../data/uploads");
 export const THUMBNAILS_DIR = path.join(UPLOADS_DIR, "thumbnails");
 // How much of the actual free space will be offered to the app as a fraction eg. 1/2
 // Currently just used for testing filled disk indicator and not hooked up to upload
